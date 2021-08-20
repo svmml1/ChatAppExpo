6e957c93859bd744db25fe7442b9bfb7d3337c5a
Sergio <sergiovmml@gmail.com>
2021-08-20 10:34:55 -0300

    1

---
+++ components/ChatRoomItem/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import {View, Text, StyleSheet, Image } from 'react-native'
+import {styles} from './styles'
+
+
+export function ChatRoomItem(props){
+  const {chatRoom} = props;
+  return(
+    <View style={styles.container}>
+      <Image 
+      source={ {uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} 
+      style={styles.image}
+      />
+      <View style={styles.badgeContainer}>
+        <Text style={styles.badgeText}> 4 </Text>
+      </View>
+      <View style={styles.rightContainer}>
+        <View style={styles.row}>
+          <Text style={styles.name}>Elon Musk</Text>
+          <Text style={styles.text}>11:11 AM</Text>
+        </View>
+        <Text 
+        ellipsizeMode="head"
+        numberOfLines={1} 
+        style={styles.text}>hola hola coca cola</Text>
+      </View>
+    </View>
+  )
+}
+
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,27 @@
 import * as React from 'react';
 import { Text, View,Image , StyleSheet} from 'react-native'
+import { ChatRoomItem } from '../components/ChatRoomItem';
+import ChatRoomsData from '../assets/dummy-data/ChatRooms';
+
+const chatRoom1 = ChatRoomsData[0];
+const chatRoom2 = ChatRoomsData[2];
 
 
 export default function TabOneScreen() {
   return (
-    <View>
-      <Image 
-      source={ {uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} 
-      style={Styles.image}
-      />
-      <View>
-        <View>
-          <Text>Elon Musk</Text>
-          <Text>11:11 AM</Text>
-        </View>
-      </View>
+    <View style={styles.page}>
+   <ChatRoomItem chatRoom={chatRoom1} name="a"/>
+   <ChatRoomItem chatRoom={chatRoom2}/>
+
     </View>
   );
 }
 
 
-const Styles = StyleSheet.create({
-    container:{
-
-    },
-    image: {
-      height: 100,
-      width: 100
-    }
+const styles = StyleSheet.create({
+    
+  page:{
+    backgroundColor: 'white'
+  }
 
 })
