ead7c9ba672daad9052759fcf6f0439d6972d2d0
Sergio <sergiovmml@gmail.com>
2021-08-21 20:03:34 -0300

    Aplify, Auth

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Text, View,Image , StyleSheet, FlatList} from 'react-native'
+import { Text, View,Image , StyleSheet, FlatList, Pressable} from 'react-native'
+import { Auth, DataStore } from 'aws-amplify';
 import { ChatRoomItem } from '../components/ChatRoomItem';
 import ChatRoomsData from '../assets/dummy-data/ChatRooms';
 
@@ -7,6 +8,13 @@ import ChatRoomsData from '../assets/dummy-data/ChatRooms';
 
 
 export function HomeScreen() {
+
+function logOut () {
+    Auth.signOut();
+  }
+
+
+
   return (
     <View style={styles.page}>
       <FlatList 
@@ -14,7 +22,11 @@ export function HomeScreen() {
       renderItem={({item}) => <ChatRoomItem chatRoom={item} /> }
       showsVerticalScrollIndicator={false}
       />
-
+      {/* <Pressable onPress={logOut} style={{backgroundColor: 'red', height: 50, margin: 10, borderRadius: 50, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Logout</Text>
+  </Pressable>*/ }
+    
+      
     </View>
   );
 }
@@ -23,7 +35,8 @@ export function HomeScreen() {
 const styles = StyleSheet.create({
     
   page:{
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    flex: 1
   }
 
 })
